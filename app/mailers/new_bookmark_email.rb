03b120402e33a2fd89a29aeb4c0ad15a9e78acf3
Lyle Dippo <ldippo421@gmail.com>
2014-11-18 12:29:47 -0500

    heroku test 2

--- app/mailers/new_bookmark_email.rb
+++ app/mailers/new_bookmark_email.rb
@@ -1,7 +1,7 @@
 class NewBookmarkEmail < ActionMailer::Base
-  default from: "postmaster@sandboxc680d29e680d450f8c313aae846bdbb5.mailgun.org"
+  default from: "info@app31249869.mailgun.org"
   def new_bookmark_email(bookmark)
 	  @bookmark = bookmark
-	  mail to: "#{bookmark.user}", subject: "Success! You did it."
+	  mail to: "#{bookmark.user.email}", subject: "Success! You did it."
   end
 end
