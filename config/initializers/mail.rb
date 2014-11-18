2022082300057f287e0a614c8cbcd2e45f98ec77
Lyle Dippo <ldippo421@gmail.com>
2014-11-18 12:22:33 -0500

    test push mailgun heroku

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  port:              587, 
-  address:           'smtp.mailgun.org',
-  user_name:         ENV['MAILGUN_SMTP_LOGIN'],
-  password:          ENV['MAILGUN_SMTP_PASSWORD'],
-  domain:            'app31249869.mailgun.org',
-  authentication:    :plain,
-  content_type:      'text/html'
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'smtp.mailgun.org',
+  :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
 
